abcd471bb3a7dd85ab3cbfffe011e6ee81ff35bb
Matej Konrad <matej.konrd@gmail.com>
2018-05-05 23:58:18 +0200

    remove default scope since it is bad practise

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,6 @@
 
 class Post < ApplicationRecord
   belongs_to :user
-  default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :title, presence: true
   validates :content, presence: true, length: { maximum: 142 }
