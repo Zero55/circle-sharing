312425944ccf673140291ff42dd5c1cb181b9224
Matej Konrad <matej.konrd@gmail.com>
2018-05-06 23:43:33 +0200

    implement

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -4,4 +4,9 @@ module PostsHelper
   def empty_posts(posts)
     'Hmm... seems like there are no posts on your feed' if posts.blank?
   end
+
+  def post_seen_class(post)
+    return 'seen' if post.seen?
+    ''
+  end
 end
