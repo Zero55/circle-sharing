1db59f81869a97c87c9ae50ad821a4981a1fa9c4
Matej Konrad <matej.konrd@gmail.com>
2018-05-06 23:43:01 +0200

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2018_05_05_220200) do
     t.string "title"
     t.text "content"
     t.integer "user_id"
+    t.string "image"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
