f7ffc875dd18c855ce0241735a779f7747058d1e
Zero5 <jankocbekus@gmail.com>
2018-05-10 04:35:42 +0200

    added desc order on post feed

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,7 +5,7 @@ class PostsController < ApplicationController
   before_action :correct_user, only: [:destroy]
 
   def index
-    @posts = Post.all
+    @posts = Post.all.order("created_at DESC")
   end
 
   def show
