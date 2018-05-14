c72beba285704539ba74a0d6516bbd178a6ad252
Zero5 <jankocbekus@gmail.com>
2018-05-14 16:04:06 +0200

    added tracking for pushed posts(delete and share counters)

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_10_013838) do
+ActiveRecord::Schema.define(version: 2018_05_10_170001) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -43,6 +43,14 @@ ActiveRecord::Schema.define(version: 2018_05_10_013838) do
     t.datetime "expire"
     t.index ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "pushed_posts", force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "user_id"
+    t.integer "pushed_num"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "relationships", force: :cascade do |t|
